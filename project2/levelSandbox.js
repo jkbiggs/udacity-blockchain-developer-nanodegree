4f210293658a5870eb3f8a3e45b43d87baba658a
Joshua Biggs <joshua.biggsk@gmail.com>
2018-11-29 13:09:47 -0800

    ESLinted the project files for consistency

--- project2/levelSandbox.js
+++ project2/levelSandbox.js
@@ -1,63 +1,63 @@
-const level = require('level'); // folder path that stores the data
-const chainDB = './chaindata';
+const level = require("level"); 
+const chainDB = "./chaindata"; // folder path that stores the data
 
 class LevelSandbox {
 	// Declaring the class constructor
-  constructor() {
-  	this.db = level(chainDB);
-  }
+	constructor() {
+		this.db = level(chainDB);
+	}
 
-  // Add data to levelDB with key and value (Promise)
-  addBlockToDB(key, value) {
-    return new Promise((resolve, reject) => {
-      this.db.put(key, value, (error) => {
-        if (error) {
-          console.log('Block ' + key + ' submission failed', error);
-          reject(error);
-        }
-        console.log('Added block #' + key);
-        resolve(value);
-      });
-    });
-  }
+	// Add data to levelDB with key and value (Promise)
+	addBlockToDB(key, value) {
+		return new Promise((resolve, reject) => {
+			this.db.put(key, value, (error) => {
+				if (error) {
+					console.log("Block " + key + " submission failed", error);
+					reject(error);
+				}
+				console.log("Added block #" + key);
+				resolve(value);
+			});
+		});
+	}
 	// Get data from levelDB with a key (Promise)
 	getBlockFromDB(key) {
-    return new Promise((resolve, reject) => {
-      this.db.get(key, (error, value) => {
-        if (error) {
-          if (error.type == 'NotFoundError') {
-            resolve(undefined);
-          } else {
-            console.log('Block ' + key + ' get failed', error);
-            reject(error);
-          }
-        } else {
-          resolve(value);
-        }
-      });
-    });
-  }
+		return new Promise((resolve, reject) => {
+			this.db.get(key, (error, value) => {
+				if (error) {
+					if (error.type == "NotFoundError") {
+						resolve(undefined);
+					} else {
+						console.log("Block " + key + " get failed", error);
+						reject(error);
+					}
+				} else {
+					resolve(value);
+				}
+			});
+		});
+	}
 
 	// Implement this method
-  getBlockHeightFromDB() {
-    return new Promise((resolve, reject) => {
-      let height = -1;
+	getBlockHeightFromDB() {
+		return new Promise((resolve, reject) => {
+			let height = -1;
 
-      this.db.createReadStream()
-        .on('data', (data) => {
-          // Count each object inserted
-          height++;
-        })
-        .on('error', (error) => {
-          // reject with erroror
-          reject(error);
-        })
-        .on('close', () => {
-          //resolve with the count value
-          resolve(height);
-        });
-    });
-  }
+			this.db.createReadStream()
+				.on("data", () => {
+					// Count each object inserted
+					height++;
+				})
+				.on("error", (error) => {
+					// reject with erroror
+					reject(error);
+				})
+				.on("close", () => {
+					//resolve with the count value
+					resolve(height);
+				});
+		});
+	}
 }
 
 module.exports = LevelSandbox;
--- project2/simpleChain.js
+++ project2/simpleChain.js
@@ -1,149 +1,149 @@
-const SHA256 = require('crypto-js/sha256');
-const LevelSandbox = require('./levelSandbox');
-const Block = require('./block');
+const SHA256 = require("crypto-js/sha256");
+const LevelSandbox = require("./levelSandbox");
+const Block = require("./block");
 
 /* ===== Blockchain Class ==========================
 |  Class with a constructor for new blockchain 		|
 |  ================================================*/
 class Blockchain {
-  constructor() {
-    this.db = new LevelSandbox();
-    this.getBlockHeight().then((height) => {
-      if (height === -1) {
-        this.addGenesisBlock();
-      }
-    })
-  }
+	constructor() {
+		this.db = new LevelSandbox();
+		this.getBlockHeight().then((height) => {
+			if (height === -1) {
+				this.addGenesisBlock();
+			}
+		});
+	}
 
-  // Add genesis block
-  addGenesisBlock() {
-    this.addBlock(new Block("Genesis Block")).then(() => console.log("Added genesis block!"))
-  }
+	// Add genesis block
+	addGenesisBlock() {
+		this.addBlock(new Block("Genesis Block")).then(() => console.log("Added genesis block!"));
+	}
 
-  // Add new block
-  async addBlock(newBlock) {
+	// Add new block
+	async addBlock(newBlock) {
 
-    // Block height
-    const height = await this.getBlockHeight();
+		// Block height
+		const height = await this.getBlockHeight();
     
-    // add genesis block if it doesn't exist
-    if (height === -1 && !this.getBlock(0)) {
-      await this.addGenesisBlock();
-    }
+		// add genesis block if it doesn't exist
+		if (height === -1 && !this.getBlock(0)) {
+			await this.addGenesisBlock();
+		}
     
-    newBlock.height = height + 1; 
-    newBlock.time = new Date().getTime().toString().slice(0,-3); // UTC timestamp
+		newBlock.height = height + 1; 
+		newBlock.time = new Date().getTime().toString().slice(0,-3); // UTC timestamp
 
-    // previous block hash
-    if(newBlock.height > 0) {
-      const previousBlock = await this.getBlock(height);
-      newBlock.previousBlockHash = previousBlock.hash;
-    }
+		// previous block hash
+		if(newBlock.height > 0) {
+			const previousBlock = await this.getBlock(height);
+			newBlock.previousBlockHash = previousBlock.hash;
+		}
 
-    // Block hash with SHA256 using newBlock and converting to a string
-    newBlock.hash = SHA256(JSON.stringify(newBlock)).toString();
+		// Block hash with SHA256 using newBlock and converting to a string
+		newBlock.hash = SHA256(JSON.stringify(newBlock)).toString();
     
-    // persist to LevelDB
-    return JSON.parse(await this.db.addBlockToDB(newBlock.height, JSON.stringify(newBlock).toString()));
-  }
+		// persist to LevelDB
+		return JSON.parse(await this.db.addBlockToDB(newBlock.height, JSON.stringify(newBlock).toString()));
+	}
 
-  // Get block height
-  async getBlockHeight() {
-    return await this.db.getBlockHeightFromDB();
-  }
+	// Get block height
+	async getBlockHeight() {
+		return await this.db.getBlockHeightFromDB();
+	}
 
-  // get block
-  async getBlock(blockHeight) {
-    return JSON.parse(await this.db.getBlockFromDB(blockHeight));
-  }
+	// get block
+	async getBlock(blockHeight) {
+		return JSON.parse(await this.db.getBlockFromDB(blockHeight));
+	}
 
-  /**************** TESTING METHODS ********************************/
+	/**************** TESTING METHODS ********************************/
 
-  // validate block
-  async validateBlock(block) {
-    if (block === undefined || block.hash === undefined) {
-        console.log("block or block.hash is undefined");
-        return false;
-    }
-    // get block hash
-    let blockHash = block.hash;
-    // remove block hash to test block integrity
-    block.hash = "";
-    // generate block hash
-    let validBlockHash = SHA256(JSON.stringify(block)).toString();
-    // reset the original block hash
-    block.hash = blockHash;
-    // Compare
-    if (blockHash == validBlockHash) {
-        return true;
-    } else {
-      console.log('Block #'+blockHeight+' invalid hash:\n'+blockHash+'<>'+validBlockHash);
-      return false;
-    }
-  }
+	// validate block
+	async validateBlock(block) {
+		if (block === undefined || block.hash === undefined) {
+			console.log("block or block.hash is undefined");
+			return false;
+		}
+		// get block hash
+		let blockHash = block.hash;
+		// remove block hash to test block integrity
+		block.hash = "";
+		// generate block hash
+		let validBlockHash = SHA256(JSON.stringify(block)).toString();
+		// reset the original block hash
+		block.hash = blockHash;
+		// Compare
+		if (blockHash == validBlockHash) {
+			return true;
+		} else {
+			console.log("Block #"+blockHeight+" invalid hash:\n"+blockHash+"<>"+validBlockHash);
+			return false;
+		}
+	}
 
- // Validate blockchain
-  async validateChain() {
-    let block = "";
-    let errorLog = [];
-    let previousBlock = "";
-    let previousHash = "";
-    let isValidBlock = false;
+	// Validate blockchain
+	async validateChain() {
+		let block = "";
+		let errorLog = [];
+		let previousBlock = "";
+		let previousHash = "";
+		let isValidBlock = false;
 
-    const height = await this.db.getBlockHeightFromDB();
+		const height = await this.db.getBlockHeightFromDB();
     
-    // base case - chain has only 1 block
-    if (height === 0) {
-      return console.log("No errors detected");
-    }
+		// base case - chain has only 1 block
+		if (height === 0) {
+			return console.log("No errors detected");
+		}
 
-    for (let i = 1; i <= height; i++) {
-      // validate block's hash
-      block = await this.getBlock(i);
-      isValidBlock = this.validateBlock(block);
+		for (let i = 1; i <= height; i++) {
+			// validate block's hash
+			block = await this.getBlock(i);
+			isValidBlock = this.validateBlock(block);
 
-      if (!isValidBlock) {
-        errorLog.push(i);
-      }
-      // compare blocks hash link
-      previousBlock = await this.getBlock(i - 1);
-      previousHash = previousBlock.hash;
+			if (!isValidBlock) {
+				errorLog.push(i);
+			}
+			// compare blocks hash link
+			previousBlock = await this.getBlock(i - 1);
+			previousHash = previousBlock.hash;
       
-      if (block.previousBlockHash !== previousHash) {
-        errorLog.push(i);
-      }
-    }
+			if (block.previousBlockHash !== previousHash) {
+				errorLog.push(i);
+			}
+		}
 
-    if (errorLog.length > 0) {
-      console.log('Block errors = ' + errorLog.length);
-      console.log('Blocks: ' + errorLog);
-    } else {
-      console.log('No errors detected');
-    }
-  }
+		if (errorLog.length > 0) {
+			console.log("Block errors = " + errorLog.length);
+			console.log("Blocks: " + errorLog);
+		} else {
+			console.log("No errors detected");
+		}
+	}
 
-  // Utility Method to Tamper a Block for Test Validation
-  modifyBlock(height, block) {
-      return new Promise( (resolve, reject) => {
-          this.db.addBlockToDB(height, JSON.stringify(block).toString()).then((blockModified) => {
-              resolve(blockModified);
-          }).catch((err) => { console.log(err); reject(err)});
-      });
-  }
+	// Utility Method to Tamper a Block for Test Validation
+	modifyBlock(height, block) {
+		return new Promise( (resolve, reject) => {
+			this.db.addBlockToDB(height, JSON.stringify(block).toString()).then((blockModified) => {
+				resolve(blockModified);
+			}).catch((err) => { console.log(err); reject(err);});
+		});
+	}
 }
 
 /********************** TEST Implementation **************/
 let chain = new Blockchain();
 
 (function theLoop (i) {
-  setTimeout(function () {
-      let blockTest = new Block("Test Block - " + (i + 1));
-      chain.addBlock(blockTest).then((result) => {
-          console.log(result);
-          i++;
-          if (i < 10) theLoop(i);
-      });
-  }, 100);
+	setTimeout(function () {
+		let blockTest = new Block("Test Block - " + (i + 1));
+		chain.addBlock(blockTest).then((result) => {
+			console.log(result);
+			i++;
+			if (i < 10) theLoop(i);
+		});
+	}, 100);
 })(0);
 
 setTimeout(() => chain.validateChain(), 2000);
